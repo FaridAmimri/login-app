8fd27bf8f0565cbc557c1feb10105644672a2f45
Farid Amimri <farid.amimri@gmail.com>
2023-09-04 12:02:35 +0200

    Set Firebase logic

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -7,29 +7,47 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import WelcomeScreen from '../screens/WelcomeScreen'
 import LoginScreen from '../screens/LoginScreen'
 import SignUpScreen from '../screens/SignUpScreen'
+import useAuth from '../auth/useAuth'
+import HomeScreen from '../screens/HomeScreen'
 
 const Stack = createNativeStackNavigator()
 
 export default function Navigation() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Welcome'>
-        <Stack.Screen
-          name='Welcome'
-          options={{ headerShown: false }}
-          component={WelcomeScreen}
-        />
-        <Stack.Screen
-          name='Login'
-          options={{ headerShown: false }}
-          component={LoginScreen}
-        />
-        <Stack.Screen
-          name='SignUp'
-          options={{ headerShown: false }}
-          component={SignUpScreen}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+  const { user } = useAuth()
+
+  if (user) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Home'>
+          <Stack.Screen
+            name='Welcome'
+            options={{ headerShown: false }}
+            component={WelcomeScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+  } else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Home'>
+          <Stack.Screen
+            name='Home'
+            options={{ headerShown: false }}
+            component={HomeScreen}
+          />
+          <Stack.Screen
+            name='Login'
+            options={{ headerShown: false }}
+            component={LoginScreen}
+          />
+          <Stack.Screen
+            name='SignUp'
+            options={{ headerShown: false }}
+            component={SignUpScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+  }
 }
